62250653479ca6a9a15ce14d53fdc867a6e629d4
Logon123 <logon12@rediffmail.com>
2024-11-14 18:08:59 +0530

    .
    .

--- fetch_data.php
+++ fetch_data.php
@@ -4,7 +4,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 include 'db.php';
-
 // Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
