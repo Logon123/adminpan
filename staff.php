1f0d8947a07369808054d0738f515f722582d114
Logon123 <logon12@rediffmail.com>
2024-11-18 17:55:04 +0530

    .
    .

--- staff.php
+++ staff.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>  <!-- https://sites.google.com/view/hpprinter-supp0rtus  Printer Website-->
+<head>  
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
